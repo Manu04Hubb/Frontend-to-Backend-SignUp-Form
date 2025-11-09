1119c4b987be1383de36fee9981f611c0fc8e898
Manu04Hubb <emanuelodhiambo84@gmail.com>
2025-11-09 23:46:16 +0300

    Did alittle styling

--- simplePHP_CRUD_App/index.php
+++ simplePHP_CRUD_App/index.php
@@ -1,18 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+    <link rel="stylesheet" href="tstyle.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sends Post request method to readfile</title>
 </head>
+
 <body>
     <!--This page will contain a form that sends a POST request to display_user_profiles.php-->
-     <h1>Welcome to the User Dashboard</h1>
-    <p>Click below to view all registered users.</p>
+    <h1>Welcome to the User Dashboard</h1>
+    <main id="viewUsers">
+        <p>Click below to view all registered users.</p>
 
-    <form action="tsiread.php" method="POST">
-        <input type="hidden" name="action" value="show_users">
-        <button type="submit">View Users</button>
-    </form>
+        <form action="tsiread.php" method="POST">
+            <input type="hidden" name="action" value="show_users">
+            <button id="viewBtn" type="submit">View Users</button>
+        </form>
+    </main>
 </body>
+
 </html>
--- simplePHP_CRUD_App/tsicreate.php
+++ simplePHP_CRUD_App/tsicreate.php
@@ -1,30 +1,29 @@
-
 <?php
 //Handle Registation Logic
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $email = $_POST['email'];
     $password = $_POST['password'];
     $pwd_confirm = $_POST['pwd_confirm'];
-    
+
 
     require_once 'tsidb_connection.php';
     //Hash users password first
-    $password_hashed = password_hash($password,PASSWORD_DEFAULT);
+    $password_hashed = password_hash($password, PASSWORD_DEFAULT);
     //Verify the hased password
-    $verified_password = password_verify($password,$password_hashed);
+    $verified_password = password_verify($password, $password_hashed);
     // Runnning a prepare statement to insert userdata into the database
     $insert_query = "INSERT INTO tsi_user_registrations (user_name,user_email,user_password,pwd_confirm)
               VALUES(:user_name,:user_email,:user_password,:pwd_confirm);";
     $stmt = $pdo->prepare($insert_query);
-    
-    $stmt->bindParam(":user_name",$username);
-    $stmt->bindParam(":user_email",$email);
-    $stmt->bindParam(":user_password",$password_hashed);
-    $stmt->bindParam(":pwd_confirm",$verified_password);
-    
+
+    $stmt->bindParam(":user_name", $username);
+    $stmt->bindParam(":user_email", $email);
+    $stmt->bindParam(":user_password", $password_hashed);
+    $stmt->bindParam(":pwd_confirm", $verified_password);
+
     $stmt->execute();
-    
+
     $pdo = null;
     $stmt = null;
     die();
@@ -34,31 +33,38 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration Document</title>
+    <link rel="stylesheet" href="tstyle.css">
 </head>
+
 <body>
-    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="POST">
-        <label for="username">Username</label>
-        <input type="text" name="username" placeholder="Enter Username" required>
-        <br><br>
+    <h2>User Registration Form</h2>
+    <main id="userForm">
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+            <label for="username">Username</label>
+            <input type="text" name="username" placeholder="Enter Username" required>
+            <br><br>
 
-        <label for="email">E-mail</label>
-        <input type="email" name="email" placeholder="Enter Email" required>
-        <br><br>
+            <label for="email">E-mail</label>
+            <input type="email" name="email" placeholder="Enter Email" required>
+            <br><br>
 
-        <label for="password">Password</label>
-        <input type="password" name="password" placeholder="Enter Password" required>
-        <br><br>
+            <label for="password">Password</label>
+            <input type="password" name="password" placeholder="Enter Password" required>
+            <br><br>
 
-        <label for="pwd_confirm">Confirm Password</label>
-        <input type="text" name="pwd_confirm" placeholder="Confirm your password" required>
-        <br><br>
+            <label for="pwd_confirm">Confirm Password</label>
+            <input type="text" name="pwd_confirm" placeholder="Confirm your password" required>
+            <br><br>
 
-        <button type="submit" >Register</button>
-        <p>Already Have an account? <a href="tsilogin.php" target="_blank" rel="noopener noreferrer">Login</a></p>
-    </form>
+            <button type="submit">Register</button>
+            <p>Already Have an account? <a href="tsilogin.php" target="_blank" rel="noopener noreferrer">Login</a></p>
+        </form>
+    </main>
 </body>
+
 </html>
